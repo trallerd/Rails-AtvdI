4e06c174259ecd60aae58a04f4120e119a2baef0
trallerd <jessicacristec@gmail.com>
2021-03-03 00:29:45 -0300

    Create Employee Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_032245) do
+ActiveRecord::Schema.define(version: 2021_03_03_032730) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
